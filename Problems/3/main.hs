a27a4b96da924f132a32019f3cddc00a471e45ea
Jake Chitel <jchitel@gmail.com>
2017-05-30 17:30:12 -0500

    Slightly better sieve implementation, arrived at the wrong answer in about 10 minutes, but at least the sieve works

--- Problems/3/main.hs
+++ Problems/3/main.hs
@@ -5,4 +5,5 @@ import Utils.Prime
 main = putStrLn $ show getProblem3Value
 
 getProblem3Value :: Integer
-getProblem3Value = maximum $ primeGenErt $ floor $ sqrt $ fromIntegral 600851475143
+getProblem3Value = last $ takeWhile (<sqrtNum) primeGenErt
+    where sqrtNum = floor $ sqrt $ fromIntegral 600851475143
--- Utils/Prime.hs
+++ Utils/Prime.hs
@@ -1,9 +1,18 @@
 module Utils.Prime (primeGenErt) where
 
 -- Sieve of Eratosthenes. Should be suitable for some problems, but certainly not all
-primeGenErt :: Integer -> [Integer]
+primeGenErt :: [Integer]
 -- The sieve is an iterative filter. Starting with p as 2, filter out all numbers from 2p to n, then find the next number after p as the next p, and continue until there is no p available
-primeGenErt max = ertFilter 2 [3..max]
-    where ertFilter _ [] = []
-          ertFilter p list = p:(ertFilter nextP nextList)
-              where (nextP:nextList) = filter (\x -> x `mod` p /= 0) list
+
+-- so basically, an upper bound is needed so that the algorithm knows where to stop, but haskell is lazy, so we actually don't need one.
+-- we can simply start at 2, yield it as the first item of the list, and then we return the rest of all integers that are not divisible by 2.
+-- the next number is the first already filtered number that is not divisible by the previous number.
+-- so we return 2, then all integers greater than 2 that aren't divisible by 2.
+-- the next number is the first number that isn't divisible by any previous numbers, which is 3.
+-- the next number is the first number that isn't divisible by 2 or 3, which is 5.
+-- next is not divisible by 2, 3, or 5, which is 7.
+-- to make this lazy, we "stack" filter functions onto each other so that they are evaluated in ascending order
+-- so basically we return 'head list' for the input list, where the initial list is [2..], followed by 'filter ertFilter tail list'
+
+primeGenErt = 2:(ertFilter 2 [3..])
+    where ertFilter p (nextP:list) = nextP:(ertFilter nextP (filter (\x -> x `mod` p /= 0) list))
